4e124411f8c59623918bc077a24aef3da1c43fba
Fernando Monteiro <fer.monteiro.works@gmail.com>
2025-02-17 20:47:10 -0300

    Aula sobre tuplas

---
+++ 07 - Tuplas/01 - tuplas.py
@@ -0,0 +1,12 @@
+# não é a mesma coisa que lista
+# Tupla é imutável (não se altera os valores)
+frutas = ("laranja", "pera", "uva",)
+letras = tuple("python")
+numeros = tuple([1, 2, 3, 4])
+pais = ("Brasil",)
+
+# para acessar é igual às listas
+print(frutas[-1])
+
+#tambem podemos fazer o fatiamento
+print(frutas[::-1])
